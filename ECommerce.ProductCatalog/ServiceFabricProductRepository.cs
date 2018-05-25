02088c056da6518d9ed8ab05726eb938f06d8d23
Jake Messner <jmessner@osisoft.com>
2018-05-24 20:44:50 -0700

    Section 6 Checkpoint

---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+
+namespace ECommerce.ProductCatalog
+{
+    class ServiceFabricProductRepository : IProductRepository
+    {
+        private readonly IReliableStateManager _stateManager;
+        public ServiceFabricProductRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            IReliableDictionary<Guid, Product> products = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                await products.AddOrUpdateAsync(tx, product.Id, product, (id, value) => product);
+
+                await tx.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            IReliableDictionary<Guid, Product> products = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+            List<Product> result = new List<Product>();
+
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                var allProducts = await products.CreateEnumerableAsync(tx, EnumerationMode.Unordered);
+
+                using (var enumerator = allProducts.GetAsyncEnumerator()) {
+                    while (await enumerator.MoveNextAsync(CancellationToken.None))
+                    {
+                        KeyValuePair<Guid, Product> current = enumerator.Current;
+                        result.Add(current.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
